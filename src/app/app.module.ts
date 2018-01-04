479fc0cd3f5c05ce77f6fbb37ad075fc89f358d6
Huy Tran Trong <tthuy@orientsoftware.net>
2018-01-04 13:23:23 +0700

    create bone!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { HomeComponent } from './body/home/home.component';
+import { CourseComponent } from './body/course/course.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    HomeComponent,
+    CourseComponent,
   ],
   imports: [
     BrowserModule,
